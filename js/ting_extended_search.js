c830c3300245a370ed32579ac479d28d220b65bc
vagrant <mm@inlead.dk>
2014-07-10 20:32:54 +0000

    Case 3994 - Create module for extended search.

---
+++ js/ting_extended_search.js
@@ -0,0 +1,52 @@
+(function($) {
+  Drupal.behaviors.clearExtendForm = {
+    attach:function(context, settings) {
+      $('#extend-form-clear', context).click(function() {
+        $("#edit-language").val('');
+        $("#edit-type").val('');
+        $("#edit-source").val('');
+        $("#edit-creator").val('');
+        $("#edit-title").val('');
+        $("#edit-subject").val('');
+        $("#edit-search-block-form--2").val('');
+        return false;
+      });
+    }
+  };
+
+  Drupal.behaviors.clearExtendForm = {
+    attach:function(context, settings) {
+      $('#extend-form-clear', context).click(function() {
+        $("#edit-language").val('');
+        $("#edit-type").val('');
+        $("#edit-source").val('');
+        $("#edit-creator").val('');
+        $("#edit-title").val('');
+        $("#edit-subject").val('');
+        $("#edit-search-block-form--2").val('');
+        return false;
+      });
+    }
+  };
+
+  Drupal.extendedQueryDisplay = function() {
+
+    var queryText = $("input").filter("[name='search_block_form']").val()
+    var parts = [];
+    if (queryText) {
+      parts.push(queryText);
+    }
+    var val;
+    var label;
+    $('#edit-advanced .form-item').each(function (i, elem) {
+      if ((val = $('input,select', elem).val()) && (label = $('label', elem).text())) {
+        parts.push(label + " = " + val);
+        console.dir(parts);
+      }
+    });
+
+    if (parts.length > 0) {
+      $('#search-query-string').text(parts.join(Drupal.t(" AND ")));
+    }
+  };
+})(jQuery);
